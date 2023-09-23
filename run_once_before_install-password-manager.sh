cf629191d24e68eb4187e96f9191e82a9d281afb
handaya <14948292+handaya@users.noreply.github.com>
2023-09-24 01:16:02 +0900

    manage some external commands
    - bw
    - fzf
    - rg

---
+++ run_once_before_install-password-manager.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -eu
+
+PROG="$(basename $0)"
+_have() {
+    type "$1" >/dev/null 2>&1
+}
+
+if ! _have bsdtar; then
+    echo "$PROG: install bsdtar"
+    sudo apt install bsdtar
+fi
+
+if _have bw; then
+    echo "$PROG: bw found (skip)"
+else
+    echo "$PROG: install bw"
+    TMPDIR="$(mktemp -d)"
+    curl -LsS 'https://vault.bitwarden.com/download/?app=cli&platform=linux' | bsdtar -xf - -C "$TMPDIR"
+    chmod +x "$TMPDIR/bw"
+
+    test -d "$HOME/.local/bin" || mkdir -p "$HOME/.local/bin"
+    mv "$TMPDIR/bw" "$HOME/.local/bin"
+
+    rm -fr "$TMPDIR"
+fi
---
+++ run_onchange_after_external.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+
+"$HOME/.local/share/fzf/install" --xdg --key-bindings --completion --no-update-rc
