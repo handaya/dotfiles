a48c8c09e573ff58a1fe24bfe3d0bdd03d35815f
Kazuo DOI <kazuo@cysol.co.jp>
2023-09-25 13:58:51 +0900

    script: fix not use test barely
    - because it returns 1 if not matched (chezmoi treats that as failed)

--- run_onchange_after_external.sh
+++ run_onchange_after_external.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 
-test -x "$HOME/.local/share/fzf/install" && "$HOME/.local/share/fzf/install" --xdg --key-bindings --completion --no-update-rc
+if [ -x "$HOME/.local/share/fzf/install" ]; then
+    "$HOME/.local/share/fzf/install" --xdg --key-bindings --completion --no-update-rc
+fi
