272e44bc35e4258b4aa48aa552fe6535026a01de
Kazuo DOI <kazuo@cysol.co.jp>
2023-09-25 13:42:20 +0900

    fzf: forget to remove script entry

--- run_onchange_after_external.sh
+++ run_onchange_after_external.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-"$HOME/.local/share/fzf/install" --xdg --key-bindings --completion --no-update-rc
+#"$HOME/.local/share/fzf/install" --xdg --key-bindings --completion --no-update-rc
